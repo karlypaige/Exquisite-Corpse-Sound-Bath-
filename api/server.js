f8f2d75a26931a95f1e3c5f74005e0e2ec8350b9
Karly Maruyama <karlypaige@yahoo.com>
2021-03-23 17:35:29 -0700

    fixing error

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/funkySound', {
     useUnifiedTopology: true,
     useCreateIndex: true
 });
-const SoundRoute = require('./routes/sound.API');
+const SoundRoute = require('./routes/soundAPI');
 app.use(SoundRoute);
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
 console.log('Server on port', 3001);
