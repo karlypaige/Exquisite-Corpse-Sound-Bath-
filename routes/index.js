7f45a7aff4869701f35b9ff7bca667977c5ef5c5
Karly Maruyama <karlypaige@yahoo.com>
2021-03-26 00:53:53 -0700

    trying to change gets to posts to see if it changes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 var router = express.Router();
 
 /* GET home page. */
-router.use('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
