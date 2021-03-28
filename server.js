92d3291ec36b541342d5b4c6d3bac58aacf43b83
Karly Maruyama <karlypaige@yahoo.com>
2021-03-27 23:25:47 -0700

    adding static to express app

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const apiPort = 3001;
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.json());
+app.use(express.static(".client/dist"));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/funkySound', {
     useNewUrlParser: true,
