82d2c5ba47f352fee238596026bb78b050088c48
Karly Maruyama <karlypaige@yahoo.com>
2021-03-29 22:34:33 -0700

    setting up routes like lesson 21 MERN

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 const apiRoutes = require("./api");
 
-router.use("/api",apiRoutes)
+// API Routes
+router.use("/api", apiRoutes)
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
--- server.js
+++ server.js
@@ -5,16 +5,19 @@ const mongoose = require('mongoose');
 
 const app = express();
 const apiPort = process.env.PORT || 3001;
+const SoundRoute = require('./routes');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.json());
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
+app.use(SoundRoute);
 // require ('./routes/htmlRoutes')(app)
 // require ('./routes/soundAPI')(app)
 
@@ -26,6 +29,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/funkySound', {
 
 });
 
-const SoundRoute = require('./routes');
-app.use(SoundRoute);
+
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
